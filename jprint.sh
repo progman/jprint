ebe2380f0552b75bf1014d657e8055ac635aeb9a
progman <gnuplanet@gmail.com>
2015-08-22 02:05:41 +0300

    add example

--- jprint.sh
+++ jprint.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
+SCRIPT_PATH=$(which ${0});
+
+if [ -L "${SCRIPT_PATH}" ]; # link
+then
+	SCRIPT_PATH=$(readlink ${SCRIPT_PATH});
+fi
+
+cd $(dirname ${SCRIPT_PATH});
+
 export FILE="${1}";
 if [ "${FLAG_SHIFT}" == "" ];
 then
 	export FLAG_SHIFT="true";
 fi
 
-nodejs jprint.js
+if [ "${FILE}" == "" ];
+then
+	echo "example: ${0} test.json"
+	exit 0;
+fi
+
+if [ "$(which nodejs)" == "" ] && [ "$(which node)" == "" ];
+then
+	echo "ERROR: nodejs not found";
+	exit 1;
+fi
+
+if [ "$(which nodejs)" != "" ];
+then
+	nodejs jprint.js
+else
+	node jprint.js
+fi
+
+exit 0;
